d82081de7fb393f23a278078589467cb79f45c0a
kaim122 <62229430+kaim122@users.noreply.github.com>
2020-04-05 16:01:21 +0900

    more rapidly

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,10 +1,9 @@
 // A[]が走査する配列、nはその配列の長さ、keyは走査したい値
 // 計算量O(n)
+// 返り値はkeyが存在していればtrue,いなければfalse
 bool linear_search(int A[], int n, int key) {
-  for(int i = 0; i < n-1; i++) {
-    if(A[i] == key) {
-      return true;
-    }
-  }
-  return false;
+  int i = 0;
+  A[n] = key;
+  while(A[i] != key) i++;
+  return i != n;
 }
