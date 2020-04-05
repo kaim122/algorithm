9e2b4470d3a3ba15c11673d899c2000a2b91252b
kaim122 <62229430+kaim122@users.noreply.github.com>
2020-04-05 16:23:26 +0900

    Update and rename linear_search.cpp to Linear_Search.cpp

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,7 +1,7 @@
 // A[]が走査する配列、nはその配列の長さ、keyは走査したい値
 // 計算量O(n)
 // 返り値はkeyが存在していればtrue,いなければfalse
-bool linear_search(int A[], int n, int key) {
+bool Linear_Search(int A[], int n, int key) {
   int i = 0;
   A[n] = key;
   while(A[i] != key) i++;
